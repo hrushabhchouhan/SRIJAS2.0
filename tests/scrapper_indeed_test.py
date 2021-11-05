3071587ec4fc0fefa85963bbc5daf9d2ccc70c3f
KALGEE ANAND KOTAK <43135408+kalgeekotak99@users.noreply.github.com>
2021-11-04 23:12:41 -0400

    Revert "Fixed test case"
    This reverts commit 3828d34058105ac1ea26f7371a0a99c74eeeb469.

--- tests/scrapper_indeed_test.py
+++ tests/scrapper_indeed_test.py
@@ -14,14 +14,11 @@ data = ""
 
 
 def test_get_job_description_1():
-    job_title, jobs = get_job_description(resume_skills_dict,total_skills, threshold, role, location, no_of_jobs, data)
+    jobs = get_job_description(resume_skills_dict,total_skills, threshold, role, location, no_of_jobs, data)
     assert jobs is not None
 
 def test_get_job_description_2():
-    job_title, jobs = get_job_description(resume_skills_dict,total_skills, threshold, role, location, no_of_jobs, data)
+    jobs = get_job_description(resume_skills_dict,total_skills, threshold, role, location, no_of_jobs, data)
     for i in jobs:
         assert i == "res1"
 
-def test_get_job_description_3():
-    job_title, jobs = get_job_description(resume_skills_dict,total_skills, threshold, role, location, no_of_jobs, data)
-    assert job_title is not None
