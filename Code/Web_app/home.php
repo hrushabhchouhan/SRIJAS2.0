aeedfca083eb17534c8bc56e7606791425df5931
Neha Agarwal <89555333+nehaAgarwal96@users.noreply.github.com>
2021-11-29 22:12:11 -0500

    Update home.php
    Added changes for dashboard

--- Code/Web_app/home.php
+++ Code/Web_app/home.php
@@ -15,18 +15,48 @@ if(isset($_POST['but_logout'])){
 <!doctype html>
 <html>
     <head>
-    <div  style="background-image: url('jobs.jpg') ;">
-    <p style="font-size:50px">Welcome to Bright Future .</p>
+    <marquee style="font-size:65px" behavior="scroll" direction="left">Welcome to a Bright Future!</marquee>
     
-        
-    </head>
     
     <style>
+        img {
+            opacity : 0.5;
+        }
+        body {
+            background-image : url("jobs.jpg");
+            background-repeat : no-repeat;
+            -webkit-background-size: cover;
+            -moz-background-size: cover;
+            -o-background-size: cover;
+            background-size: cover;
+            font-weight : bold;
+            font-size : 30px;
+        }
+        input[type=text] {
+        padding:5px; 
+        border:2px solid #ccc; 
+        -webkit-border-radius: 5px;
+        border-radius: 5px;
+        }
 
-</style>
+        input[type=text]:focus {
+            border-color:#333;
+        }
+
+        input[type=submit] {
+            padding:5px 15px; 
+            background:#ccc; 
+            border:0 none;
+            cursor:pointer;
+            -webkit-border-radius: 5px;
+            border-radius: 5px; 
+        }
+    </style>
+    </head>
     <body>
     
         <h1> Skills matched with the Job Description </h1>
+        
         
         <?php
             $sql_query = "select DISTINCT skill_master.skill_title, skill_master.skill_count FROM skill_master INNER JOIN resume_skills on skill_master.skill_id = resume_skills.skill_id INNER JOIN resume_master on resume_master.resume_id = resume_skills.resume_id INNER JOIN user_resume on user_resume.resume_id = resume_master.resume_id INNER JOIN user_master on user_master.user_id = user_resume.user_id;";
@@ -35,23 +65,26 @@ if(isset($_POST['but_logout'])){
           
             // var_dump($result)
             $len = $result->num_rows;
+            echo "<table border='1'>
+            <tr>
+            <th> Skills</th>
+            <th> Job Count </th>
+            </tr>";
+
             if ($len > 0) {
                 while($row = $result->fetch_assoc()) {
                     
-                   echo $row["skill_title"]; 
-                  
-                    echo " : ";
-                   echo $row["skill_count"]; 
-                    echo "<br>";
+                    echo "<tr>";
+                    echo "<td>" .$row["skill_title"]. "</td>";
+                    echo "<td>" .$row["skill_count"]. "</td>";
+                    echo "</tr>";
                    
                 }
-                echo "<br>";
             }
-
-            echo "<br>";
-
-
+                echo "</table>";
+                echo "<br>";
         ?>
+            
         <form method='post' action="" >
             <input  type="submit" value="Logout" name="but_logout">
         </form>
